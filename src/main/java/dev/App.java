4e7b4ad117293870eab6e4d29608e0a4cc3d98d6
Ismail GACHA <igacha664@gmail.com>
2017-09-28 15:23:05 +0200

    fin tp maven-02

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,5 +1,7 @@
 package dev;
 
+import com.github.lalyos.jfiglet.FigletFont;
+
 /**
  * Hello world!
  *
@@ -9,6 +11,10 @@ public class App
     public static void main( String[] args )
     {
 
-        System.out.println( "Hello World!" );
+        // using default font standard.flf, obtained from maven artifact
+        String asciiArt1 = FigletFont.convertOneLine("App");
+        System.out.print(asciiArt1);
+
+ 
     }
 }
